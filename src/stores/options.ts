dd80a350a9ddbdce2c6401681dde53930b8cb454
aqualxx <jetwilton@outlook.com>
2022-10-16 21:36:27 +1300

    fix: make color modes work

--- src/stores/options.ts
+++ src/stores/options.ts
@@ -1,13 +1,20 @@
 import { defineStore } from "pinia";
-import { useColorMode, useLocalStorage } from '@vueuse/core'
+import { useColorMode, useLocalStorage, type BasicColorSchema } from '@vueuse/core'
 import { ref } from 'vue';
 
 export const useOptionsStore = defineStore("options", () => {
-    const options = useLocalStorage("options", ref({
-        colorMode: useColorMode({
+    const options = useLocalStorage("options", {
+        colorMode: ref(useColorMode({
             emitAuto: true,
-        })
-    }));
+        }))
+    });
+
+    // A janky way to fix using color modes
+    options.value.colorMode = useColorMode<BasicColorSchema>({
+        emitAuto: true,
+        initialValue: options.value.colorMode.value
+    })
+
     const apiKey = ref(useLocalStorage("apikey", "0000000000"));
 
     /**
